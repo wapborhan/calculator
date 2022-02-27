1826e8cf33078015c779180f2d7b9052079ca4ba
Borhan Uddin <borhaninfos@gmail.com>
2022-02-27 12:07:05 +0600

    fix

--- src/js/bmi.js
+++ src/js/bmi.js
@@ -1,6 +1,10 @@
 const btn = document.getElementById("calculate");
 
 btn.addEventListener("click", function () {
+  // Result Show Hide
+  var card = document.getElementById("card-res");
+  card.style.display = "inline-block";
+
   let height = document.querySelector("#height").value;
   let weight = document.querySelector("#weight").value;
 
@@ -10,7 +14,6 @@ btn.addEventListener("click", function () {
   }
 
   // BMI = weight in KG / (height in m * height in m)
-
   height = height / 100;
   height = height * 30.48;
 
@@ -18,23 +21,25 @@ btn.addEventListener("click", function () {
 
   BMI = BMI.toFixed(2);
 
-  document.querySelector("#bmi-result").innerHTML = BMI;
-
-  let status = "";
+  
 
   if (BMI < 18.5) {
-    status = "Underweight";
+    document.querySelector("#bmi-result").innerHTML = "Your BMI is: " + BMI;
+    document.querySelector(".comment").innerHTML = `You are <span id="comment">Underweight</span>`;
   }
   if (BMI >= 18.5 && BMI < 25) {
-    status = "Healthy";
+    document.querySelector("#bmi-result").innerHTML = "Your BMI is:" + BMI;
+    document.querySelector(".comment").innerHTML = `You are <span id="comment">Healthy</span>`;
   }
   if (BMI >= 25 && BMI < 30) {
-    status = "Overweight";
+    document.querySelector("#bmi-result").innerHTML = "Your BMI is:" + BMI;
+    document.querySelector(".comment").innerHTML = `You are <span id="comment">Overweight</span>`;
   }
   if (BMI >= 30) {
-    status = "Obese";
-  }
-  document.querySelector(
-    ".comment"
-  ).innerHTML = `You are <span id="comment">${status}</span>`;
-});
+    document.querySelector("#bmi-result").innerHTML = "Your BMI is:" + BMI;
+    document.querySelector(".comment").innerHTML = `You are <span id="comment">Obese</span>`;
+  } 
+  
+
+
+
